f643a6156a5bb267eb9ba9deed34c39a8ca7efe7
Hanne31 <161655012+Hanne31@users.noreply.github.com>
2024-03-28 00:46:18 +0000

    aula05/ 27/03

--- aulas/praticas/pratica01/questao07.c
+++ aulas/praticas/pratica01/questao07.c
@@ -1,23 +1,13 @@
+//7. Faça um programa em C que calcule a altura alcançada por um avião após ter percorrido uma certa distância (seno(ângulo) = altura/distância). Considere o ângulo de inclinação do avião menor ou igual a 45°.
 #include <stdio.h>
-int main() {
-   float valor_da_hora_trabalhadas = 150.00;
-   float total_horas_trabalhadas = 100;
-
-  const float IR = 0.25f;  cv
-  const float INSS = 0.11f;
-
-  float salario_bruto = valor_da_hora_trabalhadas * total_horas_trabalhadas;
-  float imposto_renda = salario_bruto * IR;
-  float imposto_previ = salario_bruto * INSS;
-  float salario_liquido = salario_bruto - imposto_renda - imposto_previ;
-
-  printf("------------------------------\n");
-  printf("  C O N T R A C H E Q U E     \n");
-  printf("------------------------------\n");
-  printf("Salario Bruto.....: R$ %8.2f\n", salario_bruto);
-  printf("Imposto Renda.....: R$ %8.2f\n", imposto_renda);
-  printf("Imposto Previ.....: R$ %8.2f\n", imposto_previ);
-  printf("Salario Liquido...: R$ %8.2f\n", salario_liquido);
-  
+#include <math.h>
+  int main() {
+    float distancia = 100;
+    float angulo = 45;
+    float altura = distancia * sin(angulo);
+    printf("A altura alcançada pelo avião é %f", altura);
+    
+  return 0;
   
 }
+
